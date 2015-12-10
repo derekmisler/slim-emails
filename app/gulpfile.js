2e669f5fe771b70d5f55261541b626cd9a442e9a
derekmisler <derekmisler@gmail.com>
2015-12-10 18:13:21 -0500

    switching to coffee script

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -7,15 +7,15 @@ var browserSync = require('browser-sync').create();
 // var eslint = require('gulp-eslint');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var babel = require('gulp-babel');
 var sourcemaps = require('gulp-sourcemaps');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
+var coffee = require('gulp-coffee');
 
 gulp.task('serve', ['copy-html', 'copy-images', 'scripts-dist', 'styles'], function() {
   gulp.watch('stylesheets/**/*.scss', ['styles']);
   // gulp.watch('js/**/*.js', ['lint']);
-  gulp.watch('js/**/*.js', ['scripts-dist']);
+  // gulp.watch('js/**/*.coffee', ['scripts-dist']);
   gulp.watch('*.html', ['copy-html']);
   gulp.watch('./dist/**/*').on('change', browserSync.reload);
   
@@ -25,19 +25,9 @@ gulp.task('serve', ['copy-html', 'copy-images', 'scripts-dist', 'styles'], funct
 });
 
 gulp.task('lint', function () {
-  // ESLint ignores files with "node_modules" paths.
-  // So, it's best to have gulp ignore the directory as well.
-  // Also, Be sure to return the stream from the task;
-  // Otherwise, the task may end before the stream has finished.
   return gulp.src(['**/*.js','!node_modules/**'])
-    // eslint() attaches the lint output to the "eslint" property
-    // of the file object so it can be used by other modules.
     .pipe(eslint())
-    // eslint.format() outputs the lint results to the console.
-    // Alternatively use eslint.formatEach() (see Docs).
     .pipe(eslint.format())
-    // To have the process exit with an error code (1) on
-    // lint error, return the stream and pipe to failAfterError last.
     .pipe(eslint.failAfterError());
 });
 
@@ -50,9 +40,9 @@ gulp.task('dist', [
 ]);
 
 gulp.task('scripts-dist', function() {
-  gulp.src('js/**/*.js')
+  gulp.src('js/**/*.coffee')
       .pipe(sourcemaps.init())
-      .pipe(babel())
+      .pipe(coffee())
       .pipe(concat('scripts.min.js'))
       .pipe(uglify())
       .pipe(sourcemaps.write())
